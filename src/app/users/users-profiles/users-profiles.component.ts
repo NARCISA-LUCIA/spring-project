cd28ea7bdd34aad328da4d28676b4758a1146745
Narcisa <rusnarcisa16@gmail.com>
2022-06-16 11:35:24 +0300

    Changes to the left container replaced input with div

--- src/app/users/users-profiles/users-profiles.component.ts
+++ src/app/users/users-profiles/users-profiles.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-
+import { User } from 'src/app/model/user.model';
+import { UserService } from 'src/app/service/user-service';
 
 @Component({
   selector: 'app-users-profiles',
   templateUrl: './users-profiles.component.html',
-  styleUrls: ['./users-profiles.component.css']
+  styleUrls: ['./users-profiles.component.css'],
 })
 export class UsersProfilesComponent implements OnInit {
   formControlGroup: FormGroup = this.formBuilder.group({
@@ -15,9 +16,15 @@ export class UsersProfilesComponent implements OnInit {
     password: new FormControl(''),
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.userService.getUser(1).subscribe((result: User) => {
+      if (result) {
+      }
+    });
+  }
 }
-
-
